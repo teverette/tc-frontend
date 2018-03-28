444d1265d743fc89238861d3e4ac53bab6142a09
Tre Everette <tre.everette@gmail.com>
2018-03-27 21:28:24 -0500

    Add missing practice question and update navigation.

--- practice-8.php
+++ practice-8.php
@@ -1,85 +1,99 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Which choice creates the most suspense?</title>
-  	  <?php include '_stylesheet.html'; ?>
+	<head>
+	  <title>Which story uses situational irony?</title>
+	  <?php include '_stylesheet.html'; ?>
 	  <?php include '_js.html'; ?>
-</head>
+	  <style>
+	  	
+	  </style>
+	</head>
 <body class="practice">
-
-<div class="content"> 
+	<div class="content"> 
 	<div class="content-header content-header-practice">Practice</div>
-	
 	<div class="grid-x grid-padding-x">
-	  <h2 class="title medium-12">Which choice creates the most <span data-tooltip aria-haspopup="true" style="color:#4A90E2;" data-disable-hover="false" tabindex="1" 
-			      	title="To arrange information and ideas in to a pattern.">suspense</span>?</h2>
-	 
-	  <div class="medium-12 grid-x lesson-copy">
-	    <p>Joan had been studying for the spelling bee all year. Her parents had quizzed her at dinner every night. Her teachers had given her extra homework to
-	     help her prepare. By the time the real competition started, she thought she was ready.  After eight rounds, only she and one boy remained. Joan listened
-	      as his word was read—and as he misspelled his word! Although Joan had listened carefully to the mistake her final competitor made, she was still not sure 
-	      how to spell the word. Joan asked the announcer to use the word in a sentence.  She was less concerned about what the word meant, she needed more time to 
-	      think about how the word was spelled. If Joan spelled her word correctly, she would win. Joan could sense that the audience knew she was stalling. She 
-	      looked back at her competitor only to find a smirk on his face. If she did not spell the word correctly, her competitor would have a chance to win the 
-	      spelling bee. Joan became nervous and could feel the worry as sensations throughout her body. Her hands were sweaty, heart was fluttering, and her mouth 
-	      was very dry. Joan could hear the announcer finishing the sentence she had requested. Joan paused for a moment. The room was completely silent. Joan 
-	      closed her eyes and began to spell. After Joan completed spelling the word, she opened her eyes only to see the emotionless faces of the spelling bee judges.
-	    </p>
-	    <p>“That is incorrect,” the announcer said. Joan took a step back and hung her head as her final competitor approached the microphone. Joan couldn’t believe 
-	    it. Was she going to lose the spelling bee?  Had all of her hard work not been enough to win first place? Thoughts of late-night studying and missing out on 
-	    time with friends clouded her mind. Joan fought back her tears and refocused her attention. If her competitor spelled the word correctly then the rounds would 
-	    continue until one of them could spell a word that the other could not. Joan listened carefully as the announcer called the next word to be spelled. Joan lifted 
-	    her head with excitement as she was certain how to spell the word if given the opportunity. She watched as  
-	    “<a class="button dropdown-question" data-question="q8_1" data-drawer-id="drawer-one" onclick="showDrawer(this);">Select an option</a>"  
-	    </p>
+	  
+	  <h2 class="title medium-12">Which example uses 
+	  	<span data-tooltip has-tip aria-haspopup="true" class="has-tip" style="color:#4A90E2" data-disable-hover="false" tabindex="1" title="Fear or excitement of the unknown in a narrative.">suspense</span>?</h2>
+	  <div class="grid-x medium-12" style="position:relative">
+	  	<label class="radio">
+			<div class="mc-input-box" style="top: 2px">
+		  		<input type="radio" class="q8_1 question" name="q8_1" value="1"/>
+		  	</div>
+		  	<p class="lesson-copy mc-text">A boy rises from his bed, stretches his arms wide, and is ready to start the day.</p>
+		</label>
+		<label class="radio">
+			<div class="mc-input-box" style="top: 81px">
+		  		<input type="radio" class="q8_1 question" name="q8_1" value="2"/>
+		  	</div>
+		  	<p class="lesson-copy mc-text">A woman searches everywhere for her hat, which is on her head.</p>
+		</label>
+	  
+	  	<label class="radio">
+	  		<div class="mc-input-box" style="top: 161px">
+	  			<input  type="radio" class="q8_1 question" name="q8_1" value="3"/>
+	  		</div>
+	  		<p class="lesson-copy mc-text">Two runners have been racing for first place for an hour. As they round the corner to the finish line, they run into each other.</p>
+	  	</label>
+	  	<label class="radio">
+	  		<div class="mc-input-box" style="top: 267px">
+	  			<input  type="radio" class="q8_1 question" name="q8_1" value="4"/>
+	  		</div>
+	  		<p class="lesson-copy mc-text">A waiter drops a tray of food and a customer says, “Nice catch.”</p>
+	  	</label>
 	  </div>
-	  <input type="hidden" class="q8_1 dd-response" name="q8_1"></input>
+	  
 	</div>
 	<div class="rationale-sidebar"></div>
-</div>
-<?php $width = "11.1"; ?>
-<?php $back = "practice-7.php"; ?>
-<?php include '_footer_progress.php'?>
-
-<div id="drawer-one" class="drop-down-drawer q8_1 text-center" style="height: 128px;">
-	<a class="button secondary answer-option q8_1" data-value="1">her competitor took a deep breath and began to spell.</a>
-	<a class="button secondary answer-option q8_2" data-value="2">her competitor misspelled the word.</a>
-	<a class="button secondary answer-option q8_3" data-value="3">her competitor looked around the room.</a>	
-</div>
-
-</div>
+	</div>
+		<?php $width = "89.9"; ?>
+		<?php $back = "practice-7.php"; ?>
+		<?php include '_footer_progress.php'?>
+	
  <script>
-      	$(document).foundation();
-      	var answerKey = {q8_1:"1"};
+      $(document).foundation();
+      /* setup question and answer for quiz question */
+      	var questions = ["q8_1"];
       	var hintArray = {
       	      	"hint_q8_1":{
-          	      	2:"Suspense is excitement or fear about the unknown. If Joan’s competitor misspells the word, readers know what has happened and there is no suspense.",
-          	      	3:"Suspense is excitement or fear about the unknown. Joan’s competitor looking around the room does not create more anticipation."
-              	 }
+          	      	1:"This situation does not involve fear or excitement.",
+          	      	2:"This is an example of irony, but it is not suspenseful.",
+              	    4:"This is verbal irony, but does not create a sense of anticipation."
+				}
       	}
       	var rationaleArray = {
-      	      	"rationale_q8_1":"Suspense is excitement or fear about the unknown. As Joan’s competitor begins to spell the word, \
-          	      	the reader gets excited to know if he spells it correctly."
+      	      	"rationale_q8_1":"Correct! Readers want to know who will win the race, especially since the runners have collided."
       	}
-		var nextPage = "practice-9.php";
-    
-        $(document).on("click",".check", function() {
-        	ClozeDropdown.assess();
-        });
-      
-      	$(document).on("click",".check-verified", function() {
-		  window.location.href=nextPage;
-      	});
-      
-      	var storage = window.sessionStorage;
-    	storage["quiz_total"]=0;
-      	$(document).ready(function() {
-			ClozeDropdown.initialize("practice_8",1,1);
-			if(hasAlreadyAnswered("practice_8")) {
-				ClozeDropdown.setPreviousResponse();
-			}
-	  	});
+		var answerKey = {q8_1:"3"};
+  		var nextPage = "practice-9.php";
 
+	    $(document).on("click",".check", function() {
+	    	ChoiceMatrix.assess(false);
+	    });
+    
+		$(document).on("click",".check-verified", function() {
+			window.location.href=nextPage;
+		});
+
+     	$("input:radio").click(function() {
+     		if($('.hint').is(':visible')) {
+				ChoiceMatrix.resetForm();
+			}
+  			ChoiceMatrix.enableButton();
+ 		});
+
+      	$(document).ready(function() {
+			highlightCurrentQuiz();
+			highlightAllCorrectQuiz();
+			ChoiceMatrix.initialize("practice_8",1,1,"mc");
+			if(hasAlreadyAnswered("practice_8")) {
+				ChoiceMatrix.setPreviousResponse();
+			}
+		});
+
+      	window.onload = function() {
+    		
+		}
   </script>
 </body>
 </html>
--- practice-9.php
+++ practice-9.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Which choices create suspense?</title>
+  <title>Which choice creates the most suspense?</title>
   	  <?php include '_stylesheet.html'; ?>
 	  <?php include '_js.html'; ?>
 </head>
@@ -11,20 +11,29 @@
 	<div class="content-header content-header-practice">Practice</div>
 	
 	<div class="grid-x grid-padding-x">
-	  <h2 class="title medium-12">Which choices create <span data-tooltip aria-haspopup="true" style="color:#4A90E2;" data-disable-hover="false" tabindex="1" 
+	  <h2 class="title medium-12">Which choice creates the most <span data-tooltip aria-haspopup="true" style="color:#4A90E2;" data-disable-hover="false" tabindex="1" 
 			      	title="To arrange information and ideas in to a pattern.">suspense</span>?</h2>
 	 
 	  <div class="medium-12 grid-x lesson-copy">
-	    
-	    <p>With a mile left to go in a big race, the front runner 
-	    	“<a class="button dropdown-question" data-question="q9_1" data-drawer-id="drawer-one" onclick="showDrawer(this);">Select an option</a>"  
-	    	She <a class="button dropdown-question" data-question="q9_2" data-drawer-id="drawer-two" onclick="showDrawer(this);">Select an option</a>
-	    	 She asks herself, <a class="button dropdown-question" data-question="q9_3" data-drawer-id="drawer-three" onclick="showDrawer(this);">Select an option</a>
+	    <p>Joan had been studying for the spelling bee all year. Her parents had quizzed her at dinner every night. Her teachers had given her extra homework to
+	     help her prepare. By the time the real competition started, she thought she was ready.  After eight rounds, only she and one boy remained. Joan listened
+	      as his word was read—and as he misspelled his word! Although Joan had listened carefully to the mistake her final competitor made, she was still not sure 
+	      how to spell the word. Joan asked the announcer to use the word in a sentence.  She was less concerned about what the word meant, she needed more time to 
+	      think about how the word was spelled. If Joan spelled her word correctly, she would win. Joan could sense that the audience knew she was stalling. She 
+	      looked back at her competitor only to find a smirk on his face. If she did not spell the word correctly, her competitor would have a chance to win the 
+	      spelling bee. Joan became nervous and could feel the worry as sensations throughout her body. Her hands were sweaty, heart was fluttering, and her mouth 
+	      was very dry. Joan could hear the announcer finishing the sentence she had requested. Joan paused for a moment. The room was completely silent. Joan 
+	      closed her eyes and began to spell. After Joan completed spelling the word, she opened her eyes only to see the emotionless faces of the spelling bee judges.
+	    </p>
+	    <p>“That is incorrect,” the announcer said. Joan took a step back and hung her head as her final competitor approached the microphone. Joan couldn’t believe 
+	    it. Was she going to lose the spelling bee?  Had all of her hard work not been enough to win first place? Thoughts of late-night studying and missing out on 
+	    time with friends clouded her mind. Joan fought back her tears and refocused her attention. If her competitor spelled the word correctly then the rounds would 
+	    continue until one of them could spell a word that the other could not. Joan listened carefully as the announcer called the next word to be spelled. Joan lifted 
+	    her head with excitement as she was certain how to spell the word if given the opportunity. She watched as  
+	    “<a class="button dropdown-question" data-question="q8_1" data-drawer-id="drawer-one" onclick="showDrawer(this);">Select an option</a>"  
 	    </p>
 	  </div>
-	  <input type="hidden" class="q9_1 dd-response" name="q9_1"></input>
-	  <input type="hidden" class="q9_2 dd-response" name="q9_2"></input>
-	  <input type="hidden" class="q9_3 dd-response" name="q9_3"></input>
+	  <input type="hidden" class="q8_1 dd-response" name="q8_1"></input>
 	</div>
 	<div class="rationale-sidebar"></div>
 </div>
@@ -32,46 +41,27 @@
 <?php $back = "practice-7.php"; ?>
 <?php include '_footer_progress.php'?>
 
-<div id="drawer-one" class="drop-down-drawer q9_1 text-center" style="height: 128px;">
-	<a class="button secondary answer-option q9_1" data-value="1">twists her ankle and falls.</a>
-	<a class="button secondary answer-option q9_2" data-value="2">feels great.</a>
-	<a class="button secondary answer-option q9_3" data-value="3">makes a friend.</a>	
-</div>
-<div id="drawer-two" class="drop-down-drawer q9_2 text-center" style="height: 128px;">
-	<a class="button secondary answer-option q9_1" data-value="1">watches other runners catching up.</a>
-	<a class="button secondary answer-option q9_2" data-value="2">quits immediately.</a>
-	<a class="button secondary answer-option q9_3" data-value="3">isn’t really hurt.</a>	
-</div>
-<div id="drawer-three" class="drop-down-drawer q9_3 text-center" style="height: 128px;">
-	<a class="button secondary answer-option q9_1" data-value="1">Will I be able to finish the race?</a>
-	<a class="button secondary answer-option q9_2" data-value="2">What made that hole?</a>
-	<a class="button secondary answer-option q9_3" data-value="3">What’s for dinner tonight?</a>	
+<div id="drawer-one" class="drop-down-drawer q8_1 text-center" style="height: 128px;">
+	<a class="button secondary answer-option q8_1" data-value="1">her competitor took a deep breath and began to spell.</a>
+	<a class="button secondary answer-option q8_2" data-value="2">her competitor misspelled the word.</a>
+	<a class="button secondary answer-option q8_3" data-value="3">her competitor looked around the room.</a>	
 </div>
 
 </div>
  <script>
       	$(document).foundation();
-      	var answerKey = {q9_1:"1", q9_2:"1",q9_3:"1"};
+      	var answerKey = {q8_1:"1"};
       	var hintArray = {
-      	      	"hint_q9_1":{
-          	      	2:"This does not create excitement or fear.",
-          	      	3:"This is a nice event, not something suspenseful."
-              	 },
-              	"hint_q9_2":{
-          	      	2:"If the race is over, there is no suspense.",
-          	      	3:"This is a nice event, not something suspenseful."
-              	 },
-              	"hint_q9_3":{
-          	      	2:"Who made the hole will not affect the outcome of the race, so this does not build suspense.",
-          	      	3:"This question doesn’t contribute to the suspense in the story."
+      	      	"hint_q8_1":{
+          	      	2:"Suspense is excitement or fear about the unknown. If Joan’s competitor misspells the word, readers know what has happened and there is no suspense.",
+          	      	3:"Suspense is excitement or fear about the unknown. Joan’s competitor looking around the room does not create more anticipation."
               	 }
       	}
       	var rationaleArray = {
-      	      	"rationale_q9_1":"Correct! When the runner twists her ankle, readers worry about what will happen, creating suspense.",
-      	      	"rationale_q9_2":"Correct! Readers become more worried that the runner will not win the race.",
-      	      	"rationale_q9_3":"Correct! The question that worries readers is the same that worries the runner."
+      	      	"rationale_q8_1":"Suspense is excitement or fear about the unknown. As Joan’s competitor begins to spell the word, \
+          	      	the reader gets excited to know if he spells it correctly."
       	}
-		var nextPage = "quiz-transition.php";
+		var nextPage = "practice-9.php";
     
         $(document).on("click",".check", function() {
         	ClozeDropdown.assess();
@@ -84,8 +74,8 @@
       	var storage = window.sessionStorage;
     	storage["quiz_total"]=0;
       	$(document).ready(function() {
-			ClozeDropdown.initialize("practice_9",3,3);
-			if(hasAlreadyAnswered("practice_9")) {
+			ClozeDropdown.initialize("practice_8",1,1);
+			if(hasAlreadyAnswered("practice_8")) {
 				ClozeDropdown.setPreviousResponse();
 			}
 	  	});
