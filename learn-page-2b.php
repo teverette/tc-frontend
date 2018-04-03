dc09a0add4d8273275dbc887640ae514ef64f8b7
Tre Everette <tre.everette@gmail.com>
2018-04-02 22:13:59 -0500

    Add animation to learn page (again)

--- learn-page-2b.php
+++ learn-page-2b.php
@@ -13,16 +13,16 @@
 		<div class="content">
 			<div class="content-header content-header-learn">Learn</div>
 			<div class="grid-x grid-padding-x">
-				<h2 class="title-learn">What is verbal irony?</h2>
+				<h2 class="title-learn display-none">What is verbal irony?</h2>
 				<div style="width:300px;display:inline-block">
-					<img src="images/CSK-L7-verbal-irony.gif" style="width:300px"/>
+					<img src="images/CSK-L7-verbal-irony.gif" style="width:300px" class="first-item display-none"/>
 				</div>
 				<div style="width:320px;display:inline-block;margin-left:25px; padding-top:30px">
-					<p class="what-is-example" style="display:none">Scott arrived downtown for his job interview almost 15 minutes late because his bus never arrived.
+					<p class="what-is-example first-item display-none" >Scott arrived downtown for his job interview almost 15 minutes late because his bus never arrived.
 					 While running from his apartment to the office, he dropped the folder that contained his application 
 					 into a deep puddle. “Well, this is going well,” he muttered.</p>
 				</div>
-				<div class="reason-for-example " style="padding-left:75px;margin-top:40px; display:none;">Scott’s comment, “Well, this is going well,” shows verbal irony because Scott<br/> says things are going well, but he means that things are not going well at all.</div>
+				<div class="reason-for-example display-none" style="padding-left:75px;margin-top:40px;">Scott’s comment, “Well, this is going well,” shows verbal irony because Scott<br/> says things are going well, but he means that things are not going well at all.</div>
 			</div>
 		</div>
 
@@ -34,8 +34,9 @@
 		 <script>
 		      $(document).foundation();
 		      $(document).ready(function() {
-					$(".what-is-example").toggle('fade',500);
-					setTimeout(function(){$(".reason-for-example").toggle('fade',500)},600);
+			      	$(".title-learn").toggle('fade',500);
+			      	setTimeout(function(){$(".first-item").toggle(500)},650);
+					setTimeout(function(){$(".reason-for-example").toggle('fade',500)},650);
 		      });
 	    </script>
 	</body>
