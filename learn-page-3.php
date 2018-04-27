5b4805bb1113622f9206c861fe318b2ff4d3a534
Tre Everette <tre.everette@gmail.com>
2018-04-27 02:50:24 -0500

    Append practice question to learn page 3.

--- learn-page-3.php
+++ learn-page-3.php
@@ -6,7 +6,7 @@
 	  <?php include '_js.html'; ?>
 	</head>
 	<body class="practice learn learn-page-3">
-		<div class="content">
+		<div class="content first-content">
 			<div class="content-header content-header-learn">Learn</div>
 			<div class="grid-x grid-padding-x slide-1">
 				<h2 class="title-learn item">What is dramatic irony?</h2>
@@ -33,18 +33,54 @@
 				<div class="reason-for-example" style="font-family: Roboto;	font-size: 24px; line-height: 38px; margin-top:150px"><span class="">Because the readers know about the birthday party 
 				and Marco does not, dramatic irony is created. The irony adds excitement and tension to the scene.</span>
 				</div>
+				<img src="images/DownArrow.png" class="slide-btn slide-7-btn item" />
 			</div>
 		</div>
-
-		<?php $width = "18.75"; ?>
+		<div class="content-2 item">
+			<img src="images/speaker.jpg" class="speaker item"/>
+			<div class="content"> 
+			<div class="content-header content-header-practice">Practice</div>
+			<div class="grid-x grid-padding-x">
+			  
+			  <h2 class="title medium-12">Which example uses dramatic irony?</h2>
+			  <div class="grid-x medium-12" style="position:relative">
+			  	<label class="radio">
+					<div class="mc-input-box" style="top: 2px">
+				  		<input type="radio" class="q3_1 question" name="q3_1" value="1"/>
+				  	</div>
+				  	<p class="lesson-copy mc-text">Joan is lost in the woods. She is scared and it is getting dark. 
+				  	The reader knows that her house is over the next hill, but Joan goes in the wrong direction.</p>
+				</label>
+				<label class="radio">
+					<div class="mc-input-box" style="top: 111px">
+				  		<input type="radio" class="q3_1 question" name="q3_1" value="2"/>
+				  	</div>
+				  	<p class="lesson-copy mc-text">Laura is looking for her mobile phone that she has 
+				  	left in her coat pocket. She eventually remembers where it is, and finds it before leaving her house.</p>
+				</label>
+			  
+			  	<label class="radio">
+			  		<div class="mc-input-box" style="top: 221px">
+			  			<input  type="radio" class="q3_1 question" name="q3_1" value="3"/>
+			  		</div>
+			  		<p class="lesson-copy mc-text">Lucas finishes his homework and leaves it on his desk. 
+			  		When he wakes up in the morning, it is gone. No one knows what has happened to it.</p>
+			  	</label>
+			  </div>
+			  
+			</div>
+			<div class="rationale-sidebar"></div>
+			</div>
+		</div>
 		<?php $back = "practice-2.php"; ?>
-		<?php $next = "practice-3.php"; ?>
-		<?php $btnClass = "check"; ?>
+		<?php $width = "31.25"; ?>
 		<?php include '_footer_progress.php'?>
 		
 		 <script>
 		      $(document).foundation();
+		      
 		      $(document).ready(function() {
+		    	    $(".forward-button").html('Check <i class="fas fa-lg fa-caret-right" ></i>');
 					$(".slide-1 .item").each(function() {
 						console.log("inside item each");
 						setTimeout(showElement.bind(null,this), timeDelay);
@@ -73,22 +109,43 @@
 					$(".slide-2").hide("slide", { direction: "up" }, animationDuration);
 					
 					$(".slide-3").addClass("accessed");
-					showElement($(".footer"));
+					/* showElement($(".footer"));
 					$(".footer").addClass("accessed");
 					$(".content").animate({
 					    height: "474"
 					  }, 900, function() {
 					    // Animation complete.
 					});
+					*/
 					$(".slide-btn").removeClass("active-btn");
-					$(".slide-3-btn").addClass("active-btn");
+					$(".slide-7-btn").show();
 				});
 
+		      $(".slide-7-btn").click(function () {
+					$(".slide-btn").removeClass("active-btn");
+					timeDelay=animationDuration+50;
+					$(".slide-7-btn").hide(0);
+
+					showElement($(".content-2"));
+					// $(".content").hide("slide", { direction: "up" }, 900);
+					$(".first-content .content-header").hide();
+					
+					$(".first-content").animate({
+					    height: "0", padding:"0", borderWidth:"0"
+					  }, 900, function() {
+						$(".speaker").show();
+						$("body").removeClass("learn");
+						showElement($(".footer"));
+						
+					});
+				});
+		      
 		      $('html').on ('mousewheel', function (e) {
 				    var delta = e.originalEvent.wheelDelta;
 				    var nowScrollStamp = new Date().getTime();
 				    var scrollDiff = nowScrollStamp - lastScrollStamp;
 				    if (delta > 3 && scrollDiff>=1500) {
+				    	if($(".content-2").is(":visible")) {return}
 						if($(".slide-2").is(":visible")) {
 					    	$(".slide-1").show("slide", { direction: "up" }, 900);
 							$(".slide-2").hide("slide", { direction: "down" }, 900);
@@ -105,6 +162,7 @@
 						}
 						
 				    } else if (delta < -3 && scrollDiff>=1500) {
+				    	if($(".content-2").is(":visible")) {return}
 						if($(".slide-1").is(":visible") && $(".slide-2").hasClass("accessed")) {
 							$(".slide-2").show("slide", { direction: "down" }, 450);
 							$(".slide-1").hide("slide", { direction: "up" }, 900);
@@ -136,5 +194,47 @@
 				  setTimeout(showElement.bind(null,$(".slide-2 .item-three")), timeDelay);
 			  }
 	    </script>
+	     <script>
+      $(document).foundation();
+      /* setup question and answer for quiz question */
+      	var questions = ["q3_1"];
+      	var hintArray = {
+      	      	"hint_q3_1":{
+          	      	2:"Dramatic irony is when the readers know something the characters do not.<br/><br/>Do you know more than this character?",
+          	      	3:"Dramatic irony is when the readers know something the characters do not.<br/><br/>Do you know more than this character?"
+				}
+      	}
+      	var rationaleArray = {
+      	      	"rationale_q3_1":"Joan’s choice seems tragic from the reader’s point of view, because we know something she does not."
+      	}
+		var answerKey = {q3_1:"1"};
+  		var nextPage = "practice-4.php";
+
+	    $(document).on("click",".check", function() {
+	    	ChoiceMatrix.assess(false);
+	    });
+    
+		$(document).on("click",".check-verified", function() {
+			window.location.href=nextPage;
+		});
+
+     	$("input:radio").click(function() {
+     		if($('.hint').is(':visible')) {
+				ChoiceMatrix.resetForm();
+			}
+  			ChoiceMatrix.enableButton();
+ 		});
+
+      	$(document).ready(function() {
+			highlightCurrentQuiz();
+			highlightAllCorrectQuiz();
+			ChoiceMatrix.initialize("practice_3",1,1,"mc");
+			if(hasAlreadyAnswered("practice_3")) {
+				ChoiceMatrix.setPreviousResponse();
+			}
+		});
+
+
+  </script>
 	</body>
 </html>
