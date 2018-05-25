c04513a988409d8ec2a4f91f795a8fab33995949
Tre Everette <tre.everette@gmail.com>
2018-05-24 19:32:23 -0500

    Remove extra spaces/tabs in js library.

--- js/window-scroller.js
+++ js/window-scroller.js
@@ -1,58 +1,59 @@
 /* window scroller variables and functions */
 
-					var windowPos = 0;
-					var scrollDistance = 400;
-$(document).ready(function() {				
-					
-				    $(".slide-btn-main").click(function () {
-				    	moveWindowForward();
-				    });
+var windowPos = 0;
+var scrollDistance = 400;
+
+$(document).ready(function() {					
+    $(".slide-btn-main").click(function () {
+    	moveWindowForward();
+    });
 });
-				    function getWindowTopPosition() {
-				    	var position = $(".content-window").position();
-				    	return position.top;
-				    }
 
-				    function moveWindowForward() {
-				    	var numOfSlides = -1*maxWindowPos/scrollDistance;
-				    	
-				    	var progressIncrement = 69.50/numOfSlides;
-				    	console.log("jump width: " + progressIncrement);
-				    	var currentProgressWidth = $(".progress-meter").width();
-				    	var newProgressWidth = currentProgressWidth + progressIncrement;
-				    	$(".progress-meter").width(newProgressWidth);
-				    	var newTopPosition = windowPos-scrollDistance;
-				    	if(newTopPosition > maxWindowPos) {
-							$(".content-window").animate({top:newTopPosition}, animationDuration, 'swing', function() {
-								windowPos = newTopPosition;
-						    });
-				    	} else {
-				    		showPracticeSlide();
-				    	}
-				    }
+function getWindowTopPosition() {
+	var position = $(".content-window").position();
+	return position.top;
+}
 
-				    function moveWindowReverse() {
-					    console.log("current top: " + getWindowTopPosition());
-				    	var newTopPosition = windowPos+scrollDistance;
-				    	
-				    	console.log("new top: " + newTopPosition);
-				    	if(newTopPosition>=400) {
-							// hide/disable button
-							console.log("at top of content");
-							return;
-				    	}
+function moveWindowForward() {
+	var numOfSlides = -1*maxWindowPos/scrollDistance;
+	
+	var progressIncrement = 69.50/numOfSlides;
+	console.log("jump width: " + progressIncrement);
+	var currentProgressWidth = $(".progress-meter").width();
+	var newProgressWidth = currentProgressWidth + progressIncrement;
+	$(".progress-meter").width(newProgressWidth);
+	var newTopPosition = windowPos-scrollDistance;
+	if(newTopPosition > maxWindowPos) {
+		$(".content-window").animate({top:newTopPosition}, animationDuration, 'swing', function() {
+			windowPos = newTopPosition;
+	    });
+	} else {
+		showPracticeSlide();
+	}
+}
 
-				    	$(".content-window").animate({top:newTopPosition}, animationDuration, 'swing', function() {
-				    		windowPos = newTopPosition;
-					    });
-				    }
+function moveWindowReverse() {
+    console.log("current top: " + getWindowTopPosition());
+	var newTopPosition = windowPos+scrollDistance;
+	
+	console.log("new top: " + newTopPosition);
+	if(newTopPosition>=400) {
+		// hide/disable button
+		console.log("at top of content");
+		return;
+	}
 
-				    function showPracticeSlide() {
-				    	showElement($(".content-2"));
-				    	$(".learn-footer").hide();
-				    	$(".content-borderless-window").animate({top:-554}, 500, 'swing');
-						if(question_item != null) {
-							$(".num-questions-remaining").text(10-question_item + " questions left");
-						}
-				    }
-				    /* end window scroller logic */
+	$(".content-window").animate({top:newTopPosition}, animationDuration, 'swing', function() {
+		windowPos = newTopPosition;
+    });
+}
+
+function showPracticeSlide() {
+	showElement($(".content-2"));
+	$(".learn-footer").hide();
+	$(".content-borderless-window").animate({top:-554}, 500, 'swing');
+	if(question_item != null) {
+		$(".num-questions-remaining").text(10-question_item + " questions left");
+	}
+}
+/* end window scroller logic */
